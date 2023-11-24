16b3b56e2474142beb7bffa9f3c88fb86733c7ea
Thappaya Wangsantiporn <thappaya.w@gmail.com>
2023-11-24 13:19:52 +0700

    Counting characters in string.

---
+++ src/main/kotlin/Counting.kt
@@ -0,0 +1,11 @@
+fun main() {
+    //String can be counted.
+    //Using the command "nameHere.length".
+
+    //Define a variable.
+    val someText = "Justin"
+
+    //Print it!
+    println(someText)
+    println("Name length: " + someText.length)
+}
